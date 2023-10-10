2155023f4337c93c02fc1c74917792390d3e3166
Rodrigo Michelassi <rodrigomichelassi@MBP-de-Rodrigo.home>
2023-10-09 21:50:01 -0300

    update deliver function

--- amqpFunc.c
+++ amqpFunc.c
@@ -173,7 +173,6 @@ void basicConsume(int connfd, char* queueName){
                   "\x51\x48\x4e\x63\x4a\x36\x64\x45\x59\x31\x77\xce", 44);
 }
 
-
 void basicDeliver(int connfd, char* queueName, char* message){
     /* definir o tamanho da mensagem e fila e alocar espaço para suas strings */
     uint8_t messageSize = strlen(message);
@@ -195,19 +194,26 @@ void basicDeliver(int connfd, char* queueName, char* message){
     strFila[0] = queueNameSize;
     for(int i = 0; i < queueNameSize; i++)
         strFila[i + 1] = (uint8_t)queueName[i];
-    strMessage[0] = messageSize;
-    for(int i = 0; i < messageSize; i++)
-        strMessage[i+1] = (uint8_t)message[i];
 
     memcpy(res1, str0, sizeof(str0));
     memcpy(res1 + sizeof(str0), strFila, queueNameSize + 1);
     memcpy(res1 + sizeof(str0) + queueNameSize + 1, str1, sizeof(str1));
-    memcpy(res1 + sizeof(str0) + queueNameSize + 1 + sizeof(str1), res2, sizeof(res2));
-    memcpy(res1 + sizeof(str0) + queueNameSize + 1 + sizeof(str1) + sizeof(res2), str2, sizeof(str2));
-    memcpy(res1 + sizeof(str0) + queueNameSize + 1 + sizeof(str1) + sizeof(res2) + sizeof(str2), strMessage, messageSize + 1);
-    memcpy(res1 + sizeof(str0) + queueNameSize + 1 + sizeof(str1) + sizeof(res2) + sizeof(str2) + messageSize + 1, str1, sizeof(str1));
 
     write(connfd, res1, sizeof(res1));
+    
+    /* 2. write, mensagem padrão, apenas escrever */
+    write(connfd, res2, 23);
+
+    /* 3. write, concatenamos prefixo à mensagem e a um sufixo */
+    strMessage[0] = messageSize;
+    for(int i = 0; i < messageSize; i++)
+        strMessage[i+1] = (uint8_t)message[i];
+
+    memcpy(res3, str2, sizeof(str2));
+    memcpy(res3 + sizeof(str2), strMessage, messageSize + 1);
+    memcpy(res3 + sizeof(str2) + messageSize + 1, str1, sizeof(str1));
+
+    write(connfd, res3, sizeof(res3));
 }
 
 void basicAck(int connfd){
@@ -218,3 +224,4 @@ void basicAck(int connfd){
     if(size == -1)
         close(connfd);
 }
+
