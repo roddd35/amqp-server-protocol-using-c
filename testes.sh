d4497308d1078d67355ed57314f148b8d0a34894
Rodrigo Michelassi <rodrigomichelassi@MBP-de-Rodrigo.home>
2023-10-22 16:38:35 -0300

    update testing file

--- testes.sh
+++ testes.sh
@@ -5,23 +5,36 @@ export cpuSYS
 export memUsage
 export cont
 
-clientes=0
+consumers=0
+publishers=0
 qtdDados=0
 
+# declarar a fila de nome fila
+amqp-declare-queue -q fila
 
-# a cada 5 publishers, vemos a quantidade de usos da CPU e afins
+# cadastrar 50 consumers na fila
+while [ $consumers -lt 50 ]; do
+    gnome-terminal -- bash -c "amqp-consume -q fila cat"
+done
+
+# a cada 10 publishers, vemos a quantidade de usos da CPU e afins
 # vamos ter 50 publishers no final
 # queremos fazer o teste com 50 publishers e 50 consumers
-while [ $qtdDados -lt 10 ]; do
-    while [ $clientes -lt $5 ]; do
+while [ $qtdDados -lt 5 ]; do
+    publishers=0
+    while [ $publishers -lt 10 ]; do
+        let publishers=publishers+1
         amqp-publish -r fila -b "mensagem "
-        let cont=cont+1
     done
+    echo "Quantidade de Publishers: $qtdDados"
+
     # obter dados do uso da CPU
     cpuUS=$(top -b -n 1 | grep "Cpu" | awk -F '[, ]+' '{printf "%s,%s ", $2, $3}')
-    echo "Uso da CPU por processos de usuário: $cpuUS \n"
-    cpuSYS=$(top -b -n 1 | grep "Cpu" | awk -F '[, ]+' '{printf "%s,%s" $5, $6}')
-    echo "Uso da CPU por processos de sistema: $cpuSYS \n"
+    echo "Uso da CPU por processos de usuário: $cpuUS"
+    cpuSYS=$(top -b -n 1 | grep "Cpu" | awk -F '[, ]+' '{printf "%s,%s", $5, $6}')
+    echo "Uso da CPU por processos de sistema: $cpuSYS"
     memUsage=$(free -h | grep "Mem:" | awk '{printf "%s/%s", $3, $2}')
-    echo "Uso da memória: $memUsage \n"
+    echo "Uso da memória: $memUsage"
+
+    let qtdDados=qtdDados+1
 done
